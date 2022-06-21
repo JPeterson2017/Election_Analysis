acbae57a53167c7c596303c81734e58620ec97a4
Peterson <Jordan.Peterson@empire-cat.com>
2022-06-20 22:08:35 -0700

    finish updating txt file

--- PyPoll.py
+++ PyPoll.py
@@ -48,24 +48,36 @@ with open(file_to_load) as election_data:
         candidate_votes[candidate_name] += 1
 
 
-            
+with open(file_to_save, "w") as txt_file:
+    
+    election_results = (
+        f'\nElection Results\n'
+        f'-----------------------\n'
+        f'Total Votes: {total_votes:,}\n'
+        f'--------------------------\n')
+    print(election_results, end="")
+    
 
+    
+    #save final results to text file
+    txt_file.write(election_results)
+        
     for candidate_name in candidate_votes:
         votes = candidate_votes[candidate_name]
         vote_percentage = float(votes) / float(total_votes) * 100
-       
-        print(f'{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n') 
         
+        candidate_results = (
+            f'{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n')
+        print(candidate_results)  
+        txt_file.write(candidate_results)
+
         if (votes > winning_count) and (vote_percentage > winning_percentage):
             winning_count = votes 
             winning_percentage = vote_percentage
 
             winning_candidate = candidate_name
-
-
-        
-            
-        winning_candidate_summary = (
+         
+    winning_candidate_summary = (
          f"-------------------\n"
          f"Winner: {winning_candidate}\n"
          f"Winning Vote Count: {winning_count:,}\n"
@@ -73,15 +85,7 @@ with open(file_to_load) as election_data:
          f"-------------------\n")
         
     print(winning_candidate_summary)
-        
-
-    
-    
-# print the candidate list
-#print(candidate_votes)
-
-
-
+    txt_file.write(winning_candidate_summary)
 
 # Using the with statement open the file as a text file
 #with open(file_to_save,'w') as txt_file:
